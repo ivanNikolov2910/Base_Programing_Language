c7cd7db72204313207ff987c65b18cc75563a62d
Ivan Nikolov <ivan.nikolov2910@gmail.com>
2022-02-17 10:18:30 +0200

    ex_2 lab pt1

--- ex_lab/ex_1.c
+++ ex_lab/ex_1.c
@@ -2,14 +2,19 @@
 
 int main()
 {
-    // printf("    **      **      \n");
-    // printf("  ******  ******    \n");
-    // printf("******************  \n");
-    // printf(" ****************   \n");
-    // printf("   ************     \n");
-    // printf("     ********       \n");
-    // printf("        **            ");
+    printf("    **      **      \n");
+    printf("  ******  ******    \n");
+    printf("******************  \n");
+    printf(" ****************   \n");
+    printf("   ************     \n");
+    printf("     ********       \n");
+    printf("        **          \n");
+
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d\t%d\n", a, b);
+    // scanf("A:%d   B:%d", &a, &b);      wrooong
+    printf("%d\t%d\n", a, b);
 
     return 0;
-
 }
