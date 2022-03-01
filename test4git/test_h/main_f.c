84bab9c9d134111218ff3b70fd287976360dc765
Ivan Nikolov <ivan.nikolov2910@gmail.com>
2022-03-01 14:12:21 +0200

    add first .h file and test

---
+++ test4git/test_h/main_f.c
@@ -0,0 +1,7 @@
+#include "utils.h"
+
+int main()
+{
+    main_ex1_test();
+    return 0;
+}
---
+++ test4git/test_h/utils.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_HEADER_FILE
+#define UTILS_HEADER_FILE
+#include <stdio.h>
+void main_ex1_test()
+{
+    printf("Hello World\n");
+}
+
+#endif
