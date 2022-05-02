0fe5f312c50a3b6604103b46eed573557681f71f
Ivan Nikolov <ivan.nikolov2910@gmail.com>
2022-05-02 23:02:57 +0300

    tset exam v2

--- example_test/exam.c
+++ example_test/exam.c
@@ -15,15 +15,47 @@ typedef struct Student
 
 int main(int argc, char **argv)
 {
+    int st_count = 3;
+    Student *s = (Student *)malloc(st_count * sizeof(Student));
+
     FILE *input = fopen("students.txt", "r");
     if (input == NULL)
     {
         perror(NULL);
         fclose(input);
     }
-    char buf[100];
-    while (fscanf(input, "%s", buf) == 1)
+    char buf[200];
+    int cnt = 0;
+
+    while (fgets(buf, sizeof(buf), input) != NULL)
     {
-        printf("%s\n", buf);
+        int pointer = 0;
+        // set class_nums
+        char num[6];
+        for (int i = 0; i < 5; i++)
+        {
+            num[i] = buf[i];
+        }
+        pointer = 7;
+        int class_num = strtol(num, NULL, 10);
+        s[cnt].class_num = class_num;
+
+        // set names
+        char names[50];
+        int name_ind = 0;
+        for (int i = pointer; buf[i] != ';'; i++)
+        {
+            names[name_ind] = buf[i];
+            ++name_ind;
+        }
+        // split names
+        char fi_name[16];
+        char s_name[16];
+        char f_name[16];
+
+        printf("%s\n", fi_name);
+        printf("%s\n", s_name);
+        printf("%s\n", f_name);
+        cnt++;
     }
 }
